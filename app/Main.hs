7e3dfdb8fefc430ed61e0db4dfefecbad26368b5
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-12-23 17:44:56 -0500

    smt solver for Day23

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,12 @@ module Main where
 import           Control.Monad.State
 import qualified Data.Text           as T
 import qualified Data.Text.IO        as TIO
-import           Day22
+import           Day23
 
 main :: IO ()
 main = do
   --print part1
-  print part2
+  part2' >>= print
   --print $ part2Sol test7
   --part2 . T.unpack <$> TIO.readFile "./input/Day15.txt" >>= print
   --evalStateT part1Sol (S test6 0) >>= print
--- src/Day23.hs
+++ src/Day23.hs
@@ -1,25 +1,25 @@
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Day23 where
-
-import Linear.V3
+import           Control.Monad
+import           Data.Foldable
+import           Data.Function
+import           Data.HashSet               (HashSet)
+import qualified Data.HashSet               as HS
+import           Data.Maybe
+import           Data.SBV                   (SBV)
+import qualified Data.SBV                   as SBV
+import           Data.Text                  (Text)
+import qualified Data.Text                  as T
+import qualified Data.Text.IO               as TIO
+import           Data.Void
+import           Linear.V3
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
-import Data.Void
-import           Data.Text                  (Text)
-import qualified           Data.Text                  as T
-import qualified Data.Text.IO               as TIO
-import Control.Monad
-import Data.Foldable
-import Data.Function
-import qualified Data.HashSet as HS
-import Data.HashSet (HashSet)
-import qualified Numeric.Interval as I
-import Data.Function
-import Data.List
 
 type C = V3 Integer
 type P = Parsec Void Text
@@ -33,24 +33,37 @@ parseInput = parse totP "" <$> TIO.readFile "./input/day23.txt" >>= \case
 totP :: P (HashSet Bot)
 totP = HS.fromList <$> some (bP <* (void eol <|> eof))
 bP = (,) <$ "pos=" <*> cP <* ", r=" <*> L.decimal
-cP = between "<" ">" $ (\[x,y,z] -> V3 x y z) <$> (L.signed (void "") L.decimal) `sepBy` "," 
+cP = between "<" ">" $ (\[x,y,z] -> V3 x y z) <$> (L.signed (void "") L.decimal) `sepBy` ","
 
 manhattenDist :: C -> C -> Integer
-manhattenDist (V3 x1 y1 z1) (V3 x2 y2 z2) = abs (x1 - x2) + abs (y1 - y2) +abs (z1 - z2) 
+manhattenDist (V3 x1 y1 z1) (V3 x2 y2 z2) = abs (x1 - x2) + abs (y1 - y2) +abs (z1 - z2)
 
 part1Sol hs = let (c1,r) = maximumBy (compare `on` snd) hs in length $ HS.filter (\(c2,_) -> manhattenDist c1 c2 <= r) hs
 
 part1 :: IO Int
 part1 = part1Sol <$> parseInput
 
-mkI :: Bot -> I.Interval C
-mkI (c,r) = I.inflate (V3 r r r) $ I.singleton c
-
-biggestInt :: HashSet Bot -> I.Interval C
-biggestInt hs = snd $ maximumBy (compare `on` fst) [ints ((\i -> (0,i)) $ mkI i) (map mkI (HS.toList (HS.delete i hs))) | i <-HS.toList hs]
+sbvManhattenDist :: C -> SBV Integer -> SBV Integer -> SBV Integer -> SBV Integer
+sbvManhattenDist (V3 x y z) x1 y1 z1 = absoluteValue (x0 - x1) + absoluteValue (y0 - y1) + absoluteValue (z0 - z1)
     where
-        ints = foldr (\c (n,i) -> if I.null (I.intersection c i) then (n,i) else (n+1,I.intersection c i))
+        x0 = SBV.literal x
+        y0 = SBV.literal y
+        z0 = SBV.literal z
+        absoluteValue n = SBV.ite (n SBV..< 0) (negate n) n
 
-part2Sol hs = head $ dropWhile (I.null . I.intersection bI) $ iterate (I.inflate (V3 1 1 1)) $ I.singleton (V3 0 0 0)
-    where
-        bI = biggestInt hs
+inRange :: SBV Integer -> SBV Integer -> SBV Integer -> Bot -> SBV Integer
+inRange x y z (c,r) = SBV.oneIf $ mDist SBV..<= SBV.literal r
+  where
+    mDist =  sbvManhattenDist c x y z
+
+part2Sol' :: HashSet Bot -> IO SBV.OptimizeResult
+part2Sol' hs = SBV.optimize SBV.Lexicographic $ do
+    [x, y, z] <- SBV.sIntegers ["x", "y", "z"]
+    SBV.maximize "in range of most bots" . sum $ map (inRange x y z) $ HS.toList hs
+    SBV.minimize "distance to 0" $ sbvManhattenDist 0 x y z
+
+part2' :: IO SBV.OptimizeResult
+part2' = parseInput >>= part2Sol'
+
+test :: HashSet Bot
+test = fromJust $ parseMaybe totP $ "pos=<10,12,12>, r=2\npos=<12,14,12>, r=2\npos=<16,12,12>, r=4\npos=<14,14,14>, r=6\npos=<50,50,50>, r=200\npos=<10,10,10>, r=5"
